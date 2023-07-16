1d9896465d583ca87a00d24a66752a9c31147e40
Mariusz Krych <mariuszkrych02@gmail.com>
2023-07-17 00:49:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".hamburger").classList.toggle(".close");
+  document.querySelector("nav").classList.toggle("show");
+});
