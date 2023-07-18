21dfe65e9584a7d4b15a732bd85f489f9cbbfa9e
Mariusz Krych <mariuszkrych02@gmail.com>
2023-07-18 11:17:12 +0200

    fix bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".hamburger").classList.toggle(".close");
+  document.querySelector(".hamburger").classList.toggle("close");
   document.querySelector("nav").classList.toggle("show");
 });
